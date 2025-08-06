0948a8204e89b3dbcfda4231db95333256cb3db8
Gidado Abdullateef <gidadolateef6@gmail.com>
2025-08-06 18:52:49 +0100

    minor changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://inventorylana.onrender.com",
+    origin: "http://localhost:3000",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
