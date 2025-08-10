0fb98183ea39a192c94c7312bb9b4fe3750976f1
Gidado Abdullateef <gidadolateef6@gmail.com>
2025-08-10 15:52:30 +0100

    middleware remove

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,24 +1,23 @@
-// import { NextResponse } from 'next/server';
-// import { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
-// export function middleware(request: NextRequest) {
-//   const token = request.cookies.get('token')?.value;
-//   if (!token) {
-//     return NextResponse.redirect(new URL('/login', request.url));
-//   }
+export function middleware(request: NextRequest) {
+  // const token = request.cookies.get('token')?.value;
+  // if (!token) {
+  //   return NextResponse.redirect(new URL('/login', request.url));
+  // }
+  // return NextResponse.next();
+}
 
-//   return NextResponse.next();
-// }
-
-// export const config = {
-//   matcher: [
-//     '/sales',
-//     '/users',
-//     '/customers',
-//     '/reports',
-//     '/settings',
-//     '/products',
-//     '/admin',
-//     '/dashboard',
-//   ],
-// };
+export const config = {
+  matcher: [
+    "/sales",
+    "/users",
+    "/customers",
+    "/reports",
+    "/settings",
+    "/products",
+    "/admin",
+    "/dashboard",
+  ],
+};
