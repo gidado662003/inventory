237530022340c51d03471adc53dc6fcd4743e0e8
Gidado Abdullateef <gidadolateef6@gmail.com>
2025-08-12 10:02:00 +0100

    new test

--- frontend/components/TopBar.tsx
+++ frontend/components/TopBar.tsx
@@ -2,20 +2,18 @@
 
 import { FaUserCircle } from "react-icons/fa";
 import { useAuth } from "@/app/context";
-import { useMediaQuery } from "@uidotdev/usehooks";
+// import { useMediaQuery } from "@uidotdev/usehooks";
 import Image from "next/image";
 
 export const TopBar = () => {
-  const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
+  // const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
   const now = new Date();
   const { user } = useAuth();
 
   return (
     <div className="flex items-center justify-between bg-white px-6 py-4 border-b border-gray-200 shadow-sm">
       <div className="text-sm font-medium text-gray-700 flex items-center gap-2">
-        {isSmallDevice && (
-          <Image src="/logo.svg" alt="logo" width={50} height={50} />
-        )}
+        {<Image src="/logo.svg" alt="logo" width={50} height={50} />}
         <p>{now.toDateString()}</p>
       </div>
       <div className="flex items-center gap-4">
